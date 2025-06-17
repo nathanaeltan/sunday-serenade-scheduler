f68521cdc6ea80ae01fdce00c8b50d79645e2903
Nathanael Tan <nathanaeltan@Nathanaels-MacBook-Pro.local>
2025-06-17 18:07:09 +0800

    update router

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/sunday-serenade-scheduler/">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
